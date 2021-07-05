feb00650a958eb2158f527cc1f0c33da3c522cb3
Samuel Lundberg <samuel@Samuels-MacBook-Air.local>
2021-07-05 21:51:31 +0200

    feat: save solution if it is a new record

--- main.py
+++ main.py
@@ -33,6 +33,46 @@ def counter(m):
     return count, tot
 
 
+# Takes a list of string and concatenates them to one string, each element separated by ', '
+def to_string(list_of_strings):
+    concat_string = ''
+    for l in list_of_strings:
+        concat_string += l + ', '
+    return concat_string
+
+
+# Makes a content of mcts and the concatenated result_string and writes it to path
+def my_write(path, result_string, mcts):
+    content = str(mcts) + '\n' + to_string(result_string)
+    file = open(path, "w")
+    file.write(content)
+    file.close()
+
+
+# If the number of monotris is lower than the recorded number for the given size the
+# result_string number of monotris is saved to file named 'size'.txt in the results folder
+def store_results(size, result_string, monotris):
+    path = "results/" + str(size) + ".txt"
+
+    try:
+        file = open(path, "r")
+        content = file.readlines()
+        file.close()
+
+        if len(content) == 2:
+            ref = int(content[0])
+            if monotris < ref:
+                print('found new best score of: ', monotris, ' for size: ', n)
+                my_write(path, result_string, monotris)
+            else:
+                print('did not beat old solution of: ', ref)
+        else:
+            print('Weird file at path: ', path)
+    except IOError:
+        print('did not find file at path: ', path, '. Creating a new one')
+        my_write(path, result_string, monotris)
+
+
 def main(n):
     color_matrix = solver(n, r=True)
     # color_matrix = solver(n)
@@ -48,8 +88,12 @@ def main(n):
 
     mono_tris, tot_tris = counter(color_matrix)
     print('monochromatic triangles = ', mono_tris, ', total triangles = ', tot_tris)
-    return result
+    return result, mono_tris
 
 
-res = main(7)
+# make the code run for lists
+n = 17
+res, score = main(n)
 print('solution string: ', res)
+
+store_results(n, res, score)
